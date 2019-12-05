217393469ca853de5658c99abadc0f9ea7d9b33a
Hernandes Junio de Assis <hernandesjunio@gmail.com>
2019-12-05 06:21:52 -0300

    Aula 40

--- src/app/app.routs.ts
+++ src/app/app.routs.ts
@@ -1,8 +1,10 @@
+import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 
-export const ROUTES: Routes=[
-    {path:'', component: HomeComponent},
-    {path:'about', component: AboutComponent}
+export const ROUTES: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'restaurants', component: RestaurantsComponent }
 ]
